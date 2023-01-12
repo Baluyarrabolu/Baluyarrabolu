c851d24d8ca857d2fb164912ae3b122e880519a9
Balu <116361289+Baluyarrabolu@users.noreply.github.com>
2023-01-12 12:22:59 +0530

    Delete Spring_Boot-Student-CRUD.zip_expanded/Spring_Boot-Student-CRUD directory

--- Spring_Boot-Student-CRUD.zip_expanded/Spring_Boot-Student-CRUD/src/main/java/com/balu/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.balu.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.balu.entity.Student;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student,Integer> {}
